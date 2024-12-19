c27277f4d8f469f0aabaf98f6bfc0d4b8ba61536
Vladimir Namestnikov <vova_vlad_putin_2@mail.ru>
2024-12-19 22:11:25 +0300

    first commit

---
+++ src/main/resources/static/js/individual_order.js
@@ -0,0 +1,27 @@
+let form = document.getElementById("basketForm");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var formData = new FormData(form);
+
+    fetch("/individual_order", {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("Данные успешно отправлены на сервер");
+                localStorage.removeItem('basketCount');
+                localStorage.removeItem('basket');
+                location.reload();
+            } else {
+                console.error("Произошла ошибка при отправке данных на сервер");
+            }
+        })
+        .catch(error => {
+            console.error("Ошибка при отправке данных на сервер:", error);
+        });
+});
+
+
